18c28d8abb98cd6685c4dfdf379161368f12f505
AlfaRiza <malfariza45@gmail.com>
2025-05-26 11:17:02 +0700

    chore: initial commit

---
+++ app/Models/Course.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class Course extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'slug',
+        'name',
+        'thumbnail',
+        'about',
+        'is_popular',
+        'category_id'
+    ];
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function benefits(): HasMany {
+        return $this->hasMany(CourseBenefit::class);
+    }
+
+    public function sections(): HasMany {
+        return $this->hasMany(CourseSection::class);
+    }
+
+    public function students(): HasMany {
+        return $this->hasMany(CourseStudent::class, 'course_id');
+    }
+
+    public function mentors(): HasMany {
+        return $this->hasMany(CourseMentor::class, 'course_id');
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'booking_trx_id',
+        'user_id',
+        'pricing_id',
+        'sub_total_amount',
+        'grand_total_amount',
+        'total_tax_amount',
+        'is_paid',
+        'payment_type',
+        'proof',
+        'started_at',
+        'ended_at'
+    ];
+
+    protected $casts = [
+        'started_at' => 'date',
+        'started_at' => 'date'
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function price(): BelongsTo {
+        return $this->belongsTo(Pricing::class, 'user_id');
+    }
+}
